ded317b31e6140491369de1fca876a1cf542c6cc
Reaper622 <1064416560@qq.com>
2021-03-12 14:31:26 +0800

    :sparkles: feat: Transfer Learning training & predict

---
+++ brand/data.js
@@ -0,0 +1,34 @@
+const IMAGE_SIZE = 224;
+
+const loadImg = (src) => {
+    return new Promise(resolve => {
+        const img = new Image();
+        img.crossOrigin = "anonymous";
+        img.src = src;
+        img.width = IMAGE_SIZE;
+        img.height = IMAGE_SIZE;
+        img.onload = () => resolve(img);
+    });
+};
+
+export const getInputs = async () => {
+    const loadImgs = [];
+    const labels = [];
+    for(let i = 0; i < 30; i++) {
+        ['android', 'apple', 'windows'].forEach(label => {
+            const imgP =  loadImg(`http://127.0.0.1:8080/brand/train/${label}-${i}.jpg`);
+            loadImgs.push(imgP);
+            labels.push([
+                label === 'android' ? 1 : 0,
+                label === 'apple' ? 1 : 0,
+                label === 'windows' ? 1 : 0,
+            ])
+        });
+    }
+    const inputs = await Promise.all(loadImgs);
+
+    return {
+        inputs,
+        labels
+    }
+};
